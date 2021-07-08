616141c90186e007fe7912e408afd8009bd36b77
seanjellesmark <59930036+seanjellesmark@users.noreply.github.com>
2021-07-08 17:01:11 +0100

    Match and LPImain

--- lpi_ch3_script.R
+++ lpi_ch3_script.R
@@ -12,6 +12,11 @@ library(rtrim)
 library(tidyverse)
 library(patchwork)
 library(MatchIt)
+library(rlpi)
+
+# set wd for infile creation
+
+setwd("C:/Users/seanj/OneDrive - University College London/Articles from Thesis/3. Assessing the effect of global conservation/LPI_files")
 
 # Load LPI data
 
@@ -26,14 +31,14 @@ cons<- read_excel("C:/Users/seanj/OneDrive - University College London/Articles 
   # Problem with cons variables being rounded down to 2.29999 (albeint being characters) and decimals are off, probably some Excel witchcraft.
   # Fix by converting to numeric, rounding and then convert to factor
 
-cons1 <- cons %>% 
-  mutate(across(cons_action_1:cons_action_4,
+cons <- cons %>% 
+  mutate(across(cons_action_1:Alternative_cons,
                 ~ case_when(.x == "1.1000000000000001" ~ "1.1",
                             .x == "2.2000000000000002" ~ "2.2",
                             .x == "2.2999999999999998" ~ "2.3",
                             TRUE ~ (.x))))
 
-# Check the different management types and validate whether these can be recategorised
+# Check the different management types and validate whether these can be recategorised - all of this is out dated and have been done
 
 lpi$Management_type <- factor(lpi$Management_type)
 
@@ -80,8 +85,17 @@ lpi_error <- lpi %>%
 lpi_work <- lpi %>% 
   filter(Managed != 2)
 
-# Remember to filter for other conditions so aggregated national trends are removed. Have to figure out the best way to do so
+# Remember to filter for other conditions so aggregated national trends are removed. Have to figure out the best way to do so - Update
+#  Easiest done using the Scale Field in the LPD.
 
+lpi_work %>%
+  group_by(Scale) %>% 
+  summarise(n_obs = n()) 
+ # Global            99
+ # National/Ocean  1227
+ # Regional        4867
+ # Smaller unit    8454
+ # Unknown           11
 
 # Number of managed and not not managed populations
 
@@ -120,9 +134,53 @@ lpi_work3 <- lpi_work2 %>%
 
 lpi_work4 <- left_join(lpi_work1, lpi_work3, by = "ID") 
 
+# Remove all duplicate columns (from not specifying all similar joining variables in the joins) ending with x
+
+lpi_work5 <- lpi_work4 %>% 
+  select(everything(), -ends_with(".x")) # Removes duplicate name and location variables
+
+# Remove variables where conservation management is unknown or cannot be categorized
+
+lpi_work6 <- lpi_work5 %>% 
+  filter(Management_type != "NA" | Management_type != "Unknown") %>% 
+  mutate(treatment = if_else(Managed == 1, 1, 0))# Drops 210 pops
 
 # Contruct pre-match object to check initial balance. 
-# Most variables are categorical and will be exact but potentially a few continuous (that we just created)
+# Most variables are categorical and will be exact but potentially a few continuous (that we just created) - Update: This takes forever, even 
+# when specified super simple
 
-pre_match <- matchit(Managed ~ Common_name.x + start_year, data = lpi_work4,
-                  method = "nearest", exact = "Common_name.x", distance = "glm")
+ # Make sure all variables are in right format
+
+lpi_work6$Binomial <- factor(lpi_work6$Binomial)
+
+lpi_work6$Country <- factor(lpi_work6$Country)
+
+# Add X to the year columns as the LPImain functions doesn't work otherwise
+
+lpi_work6 <- lpi_work6 %>% 
+  rename_with(.cols = '1970':'2018',function(x){paste0("X", x)})
+
+
+ # Reduce the number of rows in order to check if whether the matching method works properly without having to run forever
+
+lpi_sample <- sample_n(lpi_work6, 100)
+
+ # Reduce number of variables - should reduce time running. Updated and using full sample as the problem was with specifying multiple methods
+
+lpi_sample <- lpi_sample %>% 
+  select(treatment, Binomial, Country, ID)
+
+pre_match <- matchit(treatment ~ Species + Country, data = lpi_work6,
+                  method = "exact", distance = "glm")
+
+ # Extract matched data
+
+lpi_matched <- match.data(pre_match)
+
+# Create infile and LPImain
+
+# creatre infile and LPI main
+
+create_infile(lpi_matched, name = "lpi_matched",  start_col_name = 'X1970', end_col_name = 'X2018')
+
+lpi_matched<-LPIMain(infile = "lpi_matched_infile.txt", VERBOSE = FALSE, REF_YEAR = 1970)
